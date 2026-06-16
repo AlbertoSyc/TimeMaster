9fe0db98c682c804b13d4c522c48398b871d616f
AlbertoSyc <107851496+AlbertoSyc@users.noreply.github.com>
2026-06-16 18:35:01 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import { escenarios } from './data.js';
+import { calcularTiempoTotal } from './game.js';
+
+function init() {
+    console.log("Sistema Arcade Iniciado");
+    // Lógica para renderizar el menú inicial
+}
+
+init();

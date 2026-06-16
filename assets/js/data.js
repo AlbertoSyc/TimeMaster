335590451330ecd480feffec766f533a0648a247
AlbertoSyc <107851496+AlbertoSyc@users.noreply.github.com>
2026-06-16 18:34:10 +0200

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,8 @@
+export const escenarios = {
+    casa: [
+        { id: 1, nombre: "Lavarse dientes", lugar: "baño", tiempo: 180 },
+        { id: 2, nombre: "Desayunar", lugar: "cocina", tiempo: 600 }
+    ],
+    cole: [],
+    piscina: []
+};

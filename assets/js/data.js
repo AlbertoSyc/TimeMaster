f7ae8099eeabab317abf482b6ff9e5186700c45f
AlbertoSyc <107851496+AlbertoSyc@users.noreply.github.com>
2026-06-17 10:18:43 +0200

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,8 +1,21 @@
 export const escenarios = {
     casa: [
-        { id: 1, nombre: "Lavarse dientes", lugar: "baño", tiempo: 180 },
-        { id: 2, nombre: "Desayunar", lugar: "cocina", tiempo: 600 }
+        { id: 1, nombre: "Despertarse", lugar: "dormitorio", tiempo: 60 },
+        { id: 2, nombre: "Lavarse cara", lugar: "baño", tiempo: 120 },
+        { id: 3, nombre: "Lavarse dientes", lugar: "baño", tiempo: 180 },
+        { id: 4, nombre: "Vestirse", lugar: "dormitorio", tiempo: 300 },
+        { id: 5, nombre: "Desayunar", lugar: "cocina", tiempo: 600 },
+        { id: 6, nombre: "Peinarse", lugar: "baño", tiempo: 120 },
+        { id: 7, nombre: "Preparar mochila", lugar: "dormitorio", tiempo: 180 }
     ],
-    cole: [],
-    piscina: []
+    cole: [
+        { id: 1, nombre: "Sacar libros", lugar: "mesa", tiempo: 60 },
+        { id: 2, nombre: "Sacar estuche", lugar: "mesa", tiempo: 60 },
+        { id: 3, nombre: "Sacar punta", lugar: "mesa", tiempo: 120 }
+    ],
+    piscina: [
+        { id: 1, nombre: "Quitar ropa", lugar: "vestuario", tiempo: 180 },
+        { id: 2, nombre: "Poner bañador", lugar: "vestuario", tiempo: 120 },
+        { id: 3, nombre: "Hacer coleta", lugar: "vestuario", tiempo: 120 }
+    ]
 };
